72c0427c611ab860a632806ab56e9c61a6e1563d
Jacopo Massa <jacopomassa97@gmail.com>
2023-01-17 12:03:24 +0100

    Code refactoring

--- intent.pl
+++ intent.pl
@@ -1,4 +1,4 @@
-:-['data.pl']. % load data
+:-['data.pl', 'utils.pl']. % load data
 
 % testing predicate
 start(Tf) :- intent(gameAppOp, gamingServiceIntent, (chainToPlace, node42, [on(cloudGamingVF, coolCloud)]), Tf).
@@ -18,7 +18,7 @@ propertyExpectation(latency, From, To, T1, T2) :-
 propertyExpectation(bandwidth, From, To, T1, T2) :- 
     condition(T1, From, To, bandwidth, larger, 10, megabps, T2).
 propertyExpectation(privacy, From, To, T1, T2) :-
-    condition(T1, From, To, privacy, high, _, _, T2).    
+    condition(T1, From, To, privacy, edge, _, _, T2).    
 
 % deliveryExpectation/3 checks if the chain can be placed on the network
 deliveryExpectation(App, (chainToPlace, _, PartialPlacement), (placedVNFchain, VNFChain, FinalPlacement)) :-
@@ -41,82 +41,15 @@ hwOK(N, HWReqs, HWCaps, Placement) :-
     findall(HW, (member(on(V, N), Placement), vnf(V, HW, _)), HWs), sumlist(HWs, HWSum),
     HWSum + HWReqs =< HWCaps.
 
-condition(T1, _, _, privacy, high, _, _, T2) :- 
+condition(T1, _, _, privacy, edge, _, _, T2) :- 
     T1 = (TType, C, P),
     placeVNF([encVF], P, NewP),
     T2 = (TType, [encVF|C], NewP).
 
 condition(T1, From, To, bandwidth, larger, Value, _, T1) :-
     T1 = (_, _, P),
-    getBW(P, From, To, BW), BW >= Value.
+    getBandwidth(P, From, To, BW), BW >= Value.
 
 condition(T1, From, To, latency, smaller, Value, _, T1) :-
     T1 = (_, _, P),
-    getLat(P, From, To, Lat), Lat =< Value.
-
-getBW(_, From, To, BW) :- % node - node
-    node(From, _), node(To, _), link(From, To, _, BW).
-getBW([on(VNF,N)|Ps], From, To, BW) :- % node - VNF
-    node(From, _), link(From, N, _, TmpBW), 
-    getMinBW([on(VNF,N)|Ps], From, To, true, TmpBW, BW).
-getBW(P, From, To, BW) :- % VNF - node
-    member(on(From, _), P), node(To, _), 
-    getMinBW(P, From, To, false, inf, BW). 
-getBW(P, From, To, BW) :- % VNF - VNF
-    member(on(From, _), P), member(on(To, _), P),
-    getMinBW(P, From, To, false, inf, BW).
-
-getMinBW([on(_, M)], _, To, _, TmpBW, NewBW) :- % base case when To is a node
-    link(M, To, _, BW), NewBW is min(BW, TmpBW).
-getMinBW([on(To, _)|_], _, To, _, BW, BW). % base case when To is a VNF
-getMinBW([on(VNF,_)|Ps], From, To, false, OldMin, NewMin) :- % before From
-    dif(VNF, From), 
-    getMinBW(Ps, From, To, false, OldMin, NewMin).
-getMinBW([P1,P2|Ps], From, To, _, _, NewMin) :- % found From
-    P1 = on(From, N1), P2 = on(_, N2),
-    link(N1, N2, _, BW),
-    getMinBW([P2|Ps], From, To, true, BW, NewMin).
-getMinBW([P1,P2|Ps], From, To, true, OldMin, NewMin) :- % before To
-    P1 = on(_, N1), P2 = on(_, N2), dif(N1,N2),
-    link(N1, N2, _, BW),
-    TmpMin is min(OldMin, BW),
-    getMinBW([P2|Ps], From, To, true, TmpMin, NewMin).
-getMinBW([P1,P2|Ps], From, To, true, OldMin, NewMin) :- % when both VNF are on the same node, just go on 
-    P1 = on(_, N), P2 = on(_, N),
-    getMinBW([P2|Ps], From, To, true, OldMin, NewMin).
-
-getLat(_, From, To, Lat) :- % node - node
-    node(From, _), node(To, _), link(From, To, Lat, _).
-getLat([on(VNF,N)|Ps], From, To, Lat) :- % node - VNF
-    node(From, _), link(From, N, TmpLat, _), 
-    getPathLat([on(VNF,N)|Ps], From, To, true, TmpLat, Lat).
-getLat(P, From, To, Lat) :- % VNF - node
-    member(on(From, _), P), node(To, _), 
-    getPathLat(P, From, To, false, 0, Lat).
-getLat(P, From, To, Lat) :- % VNF - VNF
-    member(on(From, _), P), member(on(To, _), P),
-    getPathLat(P, From, To, false, 0, Lat).
-
-getPathLat([on(V, M)], _, To, _, TmpLat, NewLat) :- % base case when To is a node
-    link(M, To, Lat, _), vnf(V, _, ProcessingTime),
-    NewLat is TmpLat + Lat + ProcessingTime.
-getPathLat([on(To, _)|_], _, To, _, TmpLat, NewLat) :- % base case when To is a VNF
-    vnf(To, _, ProcessingTime), NewLat is TmpLat + ProcessingTime.
-getPathLat([on(VNF,_)|Ps], From, To, false, TmpLat, NewLat) :- % before From
-    dif(VNF, From), 
-    getPathLat(Ps, From, To, false, TmpLat, NewLat).
-getPathLat([P1,P2|Ps], From, To, _, _, NewLat) :- % found From
-    P1 = on(From, N1), P2 = on(_, N2),
-    link(N1, N2, FeatLat, _), vnf(From, _, ProcessingTime),
-    Lat is FeatLat + ProcessingTime,
-    getPathLat([P2|Ps], From, To, true, Lat, NewLat).
-getPathLat([P1,P2|Ps], From, To, true, TmpLat, NewLat) :- % before To
-    P1 = on(V, N1), P2 = on(_, N2), dif(N1,N2),
-    link(N1, N2, FeatLat, _), vnf(V, _, ProcessingTime),
-    Lat is TmpLat + FeatLat + ProcessingTime,
-    getPathLat([P2|Ps], From, To, true, Lat, NewLat).
-getPathLat([P1,P2|Ps], From, To, true, TmpLat, NewLat) :- % when both VNF are on the same node, only add processing time
-    P1 = on(V, N), P2 = on(_, N),
-    vnf(V, _, ProcessingTime), 
-    Lat is TmpLat + ProcessingTime,
-    getPathLat([P2|Ps], From, To, true, Lat, NewLat).
+    getLatency(P, From, To, Lat), Lat =< Value.
