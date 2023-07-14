4a7216905fedd1352f2706818fd87bf067db7182
jacopo-massa <jacopomassa97@gmail.com>
2023-07-14 09:46:23 +0000

    code refactoring: removed redundant code on 'levels' management, added levels/3 facts

--- src/conflicts.pl
+++ src/conflicts.pl
@@ -6,45 +6,24 @@ conflictsDetection(ConflictsAndSolutions, UnfeasibleConflicts) :-
     findall((C,S), conflict(C, S), CSs), sort(CSs, ConflictsAndSolutions),
     findall(C, member((C, unfeasible), ConflictsAndSolutions), UnfeasibleConflicts).
 
-% --- General ---
-% too general(?):
-% - we can define "mydif/2"
-% - specialise the rules (e.g., latency/stddev)
-% - hierarchy of soft/soft properties (so on boundaries)
+% --- General "intra"-property conflicts ---
+conflict((PId1,PId2), Solution) :-
+    propertyExpectation(PId1, I, Property, B1, L1, V1, _, VF, _),
+    propertyExpectation(PId2, I, Property, B2, L2, V2, _, VF, _),
+    dif(PId1, PId2), conflictingBounds((B1,B2),(V1,V2)), levels((L1,L2), (PId1,PId2), Solution).
+conflict((PId1,PId2), Solution) :-
+    propertyExpectation(PId1, I, Property, B1, L1, V1, _, _, VF),
+    propertyExpectation(PId2, I, Property, B2, L2, V2, _, _, VF),
+    dif(PId1, PId2), conflictingBounds((B1,B2),(V1,V2)), levels((L1,L2), (PId1,PId2), Solution).
 
-conflict((PId1,PId2), Solution) :-
-    propertyExpectation(PId1, I, Property, B1, hard, V1, _, VF, _),
-    propertyExpectation(PId2, I, Property, B2, soft, V2, _, VF, _),
-    dif(PId1, PId2), conflictingBounds((B1,V1),(B2,V2)),
-    Solution = (remove, [PId2]).
-conflict((PId1,PId2), Solution) :-
-    propertyExpectation(PId1, I, Property, B1, hard, V1, _, _, VF),
-    propertyExpectation(PId2, I, Property, B2, soft, V2, _, _, VF),
-    dif(PId1, PId2), conflictingBounds((B1,V1),(B2,V2)),
-    Solution = (remove, [PId2]).
-conflict((PId1,PId2), Solution) :-
-    propertyExpectation(PId1, I, Property, B1, soft, V1, _, VF, _),
-    propertyExpectation(PId2, I, Property, B2, soft, V2, _, VF, _),
-    dif(PId1, PId2), conflictingBounds((B1,V1),(B2,V2)),
-    Solution = (remove, [PId1,PId2]).
-conflict((PId1,PId2), Solution) :-
-    propertyExpectation(PId1, I, Property, B1, soft, V1, _, _, VF),
-    propertyExpectation(PId2, I, Property, B2, soft, V2, _, _, VF),
-    dif(PId1, PId2), conflictingBounds((B1,V1),(B2,V2)),
-    Solution = (remove, [PId1,PId2]).
-conflict((PId1,PId2), unfeasible) :- 
-    propertyExpectation(PId1, I, Property, B1, hard, V1, _, VF, _),
-    propertyExpectation(PId2, I, Property, B2, hard, V2, _, VF, _),
-    dif(PId1, PId2), conflictingBounds((B1,V1),(B2,V2)).
-conflict((PId1,PId2), unfeasible) :- 
-    propertyExpectation(PId1, I, Property, B1, hard, V1, _, _, VF),
-    propertyExpectation(PId2, I, Property, B2, hard, V2, _, _, VF),
-    dif(PId1, PId2), conflictingBounds((B1,V1),(B2,V2)).
+% LEVELS and related SOLUTIONS (in case of conflict)
+levels((hard, hard), _, unfeasible). % unfeasible
+levels((hard, soft), (_, PId2), (remove, [PId2])). % remove soft
+levels((soft, soft), (PId1, PId2), (remove, [PId1,PId2])). % remove both
 
 % CONFLICTING BOUNDS
-conflictingBounds((greater, L), (smaller, U)) :- L > U.
-conflictingBounds((dedicated,_), (same,_)).
-% conflictingBounds((B, _), (B,_)).
+conflictingBounds((greater, smaller), (L, U)) :- L > U.
+conflictingBounds((dedicated, same), _).
 
 
 
