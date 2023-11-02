2276a875e0a5f7dacf0aedb2a9e6f1ced74b3b7d
jacopo-massa <jacopomassa97@gmail.com>
2023-11-02 11:09:59 +0000

    conflicts among properties of different intents

--- src/conflicts.pl
+++ src/conflicts.pl
@@ -21,13 +21,13 @@ handleUnfeasibleConflicts([]).
 
 % --- General "intra"-property conflicts ---
 conflict((PId1,PId2), Solution) :-
-    propertyExpectation(PId1, I, Property, B1, L1, V1, _, VF, _),
-    propertyExpectation(PId2, I, Property, B2, L2, V2, _, VF, _),
-    dif(PId1, PId2), conflictingBounds(I, Property, (B1,B2),(V1,V2)), solution(Property, (L1,L2), (PId1,PId2), Solution).
+    propertyExpectation(PId1, I1, Property, B1, L1, V1, _, VF, _),
+    propertyExpectation(PId2, I2, Property, B2, L2, V2, _, VF, _),
+    dif(PId1, PId2), conflictingBounds(Property, (I1,I2), (B1,B2),(V1,V2)), solution(Property, (L1,L2), (PId1,PId2), Solution).
 conflict((PId1,PId2), Solution) :-
-    propertyExpectation(PId1, I, Property, B1, L1, V1, _, _, VF),
-    propertyExpectation(PId2, I, Property, B2, L2, V2, _, _, VF),
-    dif(PId1, PId2), conflictingBounds(I, Property, (B1,B2),(V1,V2)), solution((L1,L2), (PId1,PId2), Solution).
+    propertyExpectation(PId1, I1, Property, B1, L1, V1, _, _, VF),
+    propertyExpectation(PId2, I2, Property, B2, L2, V2, _, _, VF),
+    dif(PId1, PId2), conflictingBounds(Property, (I1,I2), (B1,B2),(V1,V2)), solution(Property, (L1,L2), (PId1,PId2), Solution).
 
 % levels and related SOLUTIONS (in case of conflict)
 solution(_, (hard, hard), _, unfeasible). % unfeasible
